87bd7b897cba56f9dfaf4afd01c3783cec1db822
Shubham Patel <shubhamprecious1@gmail.com>
2023-02-22 07:42:32 +0530

    impliment multiclient messaging

--- hermis/src/main.rs
+++ hermis/src/main.rs
@@ -1,17 +1,29 @@
-use tokio::{net::TcpListener, io::{AsyncReadExt, AsyncWriteExt}};
+use tokio::{net::TcpListener, io::{AsyncWriteExt, BufReader, AsyncBufReadExt}};
 
 #[tokio::main]
 async fn main(){
     let listener:TcpListener = TcpListener::bind("localhost:8080").await.unwrap();
-    let (mut socket, addr) = listener.accept().await.unwrap();
-
-    loop {
-        
-        let mut buffer = [0u8; 1024];
     
-        let bytes_read = socket.read(&mut buffer).await.unwrap();
+    loop {
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+        
+        tokio::spawn( async move {
+            let (reader, mut writer) = socket.split();
 
-        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+            let mut reader = BufReader::new(reader);
 
+            let mut line = String::new();
+
+            loop {
+
+                let bytes_read =  reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0{
+                    break;
+                }
+
+                writer.write_all(line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
     }
 }
